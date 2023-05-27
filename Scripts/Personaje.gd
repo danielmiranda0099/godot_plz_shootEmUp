f758c656e3f9f8e2323aa2b35eab6b42bbbe2528
danielmiranda0099 <danielmesa877@gmail.com>
2023-05-27 17:04:20 -0500

    [ADD] Character Movement

--- Scripts/Personaje.gd
+++ Scripts/Personaje.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 export (int) var velocidad = 600
 
+onready var playback = $AnimationTree.get("parameters/playback")
+
 var movimiento = Vector2(0, 0)
 
 func get_inputs():
@@ -15,9 +17,14 @@ func get_inputs():
 
 	if Input.is_action_pressed("move_right"):
 		movimiento.x += velocidad
+		playback.travel("move_right")
 
 	if Input.is_action_pressed("move_left"):
 		movimiento.x -= velocidad
+		playback.travel("move_left")
+	
+	if movimiento == Vector2():
+		playback.start("RESET")
 
 func _physics_process(_delta):
 	get_inputs()
