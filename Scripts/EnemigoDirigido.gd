f275446ae52386bd4023013c897bb28588b4606c
danielmiranda0099 <danielmesa877@gmail.com>
2023-10-06 15:00:06 -0500

    [OPTIMIZE] Motion and Memory

--- Scripts/EnemigoDirigido.gd
+++ Scripts/EnemigoDirigido.gd
@@ -6,6 +6,8 @@ export (int) var puntos = 100
 onready var personaje = get_tree().get_nodes_in_group("personaje")
 onready var posicion_personaje = personaje[0].global_position
 
+var is_in_screen = false
+
 func set_explosion():
 	collision_mask = 0
 	collision_layer = 0
@@ -14,9 +16,14 @@ func set_explosion():
 	queue_free()
 
 func _physics_process(delta):
-	posicion_personaje = personaje[0].global_position
-	global_position = global_position.move_toward(posicion_personaje, velocidad*delta)
+	if is_in_screen:
+		posicion_personaje = personaje[0].global_position
+		global_position = global_position.move_toward(posicion_personaje, velocidad*delta)
 
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_entered():
+	is_in_screen = true
--- Scripts/GrupoEnemigos.gd
+++ Scripts/GrupoEnemigos.gd
@@ -3,14 +3,20 @@ extends Path2D
 export (float) var speed = 300
 
 var recorridos
+var is_in_screen = false
 
 func _ready():
 	recorridos = get_children()
 
 func _physics_process(delta):
-	for recorrido in recorridos:
-		recorrido.offset += speed*delta
-
+	if is_in_screen:
+		for recorrido in recorridos:
+			if recorrido.get_class() == "PathFollow2D":
+				recorrido.offset += speed*delta
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_entered():
+	is_in_screen = true
